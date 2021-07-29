ae50b86982c6ebcd265ba7d8d6b7833b605fa578
王铭顺 <553147551@qq.com>
2021-07-29 21:59:03 +0800

    update

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,14 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
-
+  {
+    path: '/',
+    name:'home',
+    component:()=>import('@/views/home.vue')
+  }
 ]
 
 const router = new VueRouter({
---
+++ src/views/home.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="container">
+    <el-container>
+      <el-aside width="220px">旅梦后台管理系统</el-aside>
+      <el-container>
+        <el-header>Header</el-header>
+        <el-main>Main</el-main>
+      </el-container>
+    </el-container>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="less">
+.el-header {
+  background-color: #fff;
+  color: #333;
+  text-align: center;
+  line-height: 60px;
+}
+.el-aside {
+  background-color: #282E38;
+  height: 100vh;
+  color: #fff;
+  text-align: center;
+  line-height: 60px;
+}
+.el-main{
+  background-color: #F0F2F7;
+}
+</style>
