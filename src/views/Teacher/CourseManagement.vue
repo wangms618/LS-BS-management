3dc6d6832aebab9222aa0c148331d397991eddc4
李慧 <1622967498@qq.com>
2021-08-06 16:18:40 +0800

    8.6

--- src/views/Teacher/CourseManagement.vue
+++ src/views/Teacher/CourseManagement.vue
@@ -1,98 +1,44 @@
 <template>
-  <el-tabs v-model="activeName" @tab-click="handleClick">
-    <el-tab-pane label="课程管理" name="first">
-      <el-table
-        ref="multipleTable"
-        :data="tableData"
-        tooltip-effect="dark"
-        style="width: 100%"
-        @selection-change="handleSelectionChange"
-      >
-        <el-table-column type="selection" width="55"> </el-table-column>
-        <el-table-column label="日期" width="120">
-          <template slot-scope="scope">{{ scope.row.date }}</template>
-        </el-table-column>
-        <el-table-column prop="name" label="姓名" width="120">
-        </el-table-column>
-        <el-table-column prop="address" label="地址" show-overflow-tooltip>
-        </el-table-column>
-      </el-table>
-      <div style="margin-top: 20px">
-        <el-button @click="toggleSelection([tableData[1], tableData[2]])"
-          >切换第二、第三行的选中状态</el-button
-        >
-        <el-button @click="toggleSelection()">取消选择</el-button>
-      </div>
-    </el-tab-pane>
-    <el-tab-pane label="配置管理" name="second">配置管理</el-tab-pane>
-    <el-tab-pane label="角色管理" name="third">角色管理</el-tab-pane>
-  </el-tabs>
+  <div class="container">
+    <div class="top">
+      <i class="el-icon-document" style="color: #ffd04b"></i>
+    <span class="top-title">课程管理</span>
+    </div>
+    <div class="header">
+      <search-box>
+        <span slot="search-category">课程名称</span>
+      </search-box>
+    </div>
+    <div class="main">
+      <el-tabs type="border-card" >
+        <el-tab-pane label="用户管理">用户管理</el-tab-pane>
+        <el-tab-pane label="配置管理">配置管理</el-tab-pane>
+        <el-tab-pane label="角色管理">角色管理</el-tab-pane>
+      </el-tabs>
+    </div>
+  </div>
 </template>
 
 <script>
+import SearchBox from "@/components/Common/SearchBox.vue";
 export default {
-  data() {
-    return {
-      activeName: "second",
-      tableData: [
-        {
-          date: "2016-05-03",
-          name: "王小虎",
-          address: "上海市普陀区金沙江路 1518 弄",
-        },
-        {
-          date: "2016-05-02",
-          name: "王小虎",
-          address: "上海市普陀区金沙江路 1518 弄",
-        },
-        {
-          date: "2016-05-04",
-          name: "王小虎",
-          address: "上海市普陀区金沙江路 1518 弄",
-        },
-        {
-          date: "2016-05-01",
-          name: "王小虎",
-          address: "上海市普陀区金沙江路 1518 弄",
-        },
-        {
-          date: "2016-05-08",
-          name: "王小虎",
-          address: "上海市普陀区金沙江路 1518 弄",
-        },
-        {
-          date: "2016-05-06",
-          name: "王小虎",
-          address: "上海市普陀区金沙江路 1518 弄",
-        },
-        {
-          date: "2016-05-07",
-          name: "王小虎",
-          address: "上海市普陀区金沙江路 1518 弄",
-        },
-      ],
-      multipleSelection: [],
-    };
-  },
-  methods: {
-    handleClick(tab, event) {
-      console.log(tab, event);
-    },
-    toggleSelection(rows) {
-      if (rows) {
-        rows.forEach((row) => {
-          this.$refs.multipleTable.toggleRowSelection(row);
-        });
-      } else {
-        this.$refs.multipleTable.clearSelection();
-      }
-    },
-    handleSelectionChange(val) {
-      this.multipleSelection = val;
-    },
+  components: {
+    SearchBox,
   },
 };
 </script>
 
-<style>
+<style lang="less" scoped>
+.top {
+  .top-title {
+    margin-left: 10px;
+    font-weight: bold;
+  }
+}
+.header{
+  margin-top: 20px;
+}
+.main{
+  margin-top: 40px;
+}
 </style>
